118ceb6f27b20b7561c7924f3aff2881c01b5f04
Raa0404 <piyushrana007.pr@gmail.com>
2025-07-07 21:19:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+
+import React from "react";
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '40vh' }}>
-      <h1>OTS Final App - Full Functional Version</h1>
-      <p>This placeholder will be replaced by interactive UI for all 3 phases.</p>
+    <div>
+      <h1>OTS Final App - All Three Phases Functional</h1>
     </div>
   );
 }
